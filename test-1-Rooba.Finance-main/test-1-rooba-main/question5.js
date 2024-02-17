69aa5e555bc3b5809f9f737a9b13b5210c809c9c
saurabh-yadav7797 <159050756+saurabh-yadav7797@users.noreply.github.com>
2024-02-17 21:00:45 +0530

    Question5.js

--- test-1-Rooba.Finance-main/test-1-rooba-main/question5.js
+++ test-1-Rooba.Finance-main/test-1-rooba-main/question5.js
@@ -1,26 +1,28 @@
 db.users.aggregate([
     {
-      $group: {
-        _id: null,
-        totalUsers: { $sum: 1 },
-        averageAge: { $avg: "$age" },
-        usersByCountry: {
-          $push: {
-            country: "$country",
-            count: 1
-          }
+        $group: {
+            _id: null,
+            totalUsers: { $sum: 1 },
+            averageAge: { $avg: "$age" },
+            usersByCountry: { $push: { country: "$country", count: 1 } }
         }
-      }
     },
     {
-      $unwind: "$usersByCountry"
+        $unwind: "$usersByCountry"
     },
     {
-      $group: {
-        _id: "$usersByCountry.country",
-        totalUsers: { $sum: "$usersByCountry.count" },
-        averageAge: { $avg: "$averageAge" }
-      }
+        $group: {
+            _id: "$usersByCountry.country",
+            totalUsers: { $sum: "$usersByCountry.count" },
+            averageAge: { $first: "$averageAge" }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            country: "$_id",
+            totalUsers: 1,
+            averageAge: 1
+        }
     }
-  ])
-  
+]);
